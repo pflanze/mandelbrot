5994f38e5ab1663014a76928cbd663f84e0f4ad9
Christian Jaeger <chrjae@gmail.com>
2013-02-17 16:50:50 -0500

    cleanup (cosmetics)

--- main.hs
+++ main.hs
@@ -7,6 +7,8 @@ import Control.Parallel
 import Control.Parallel.Strategies
 
 
+-- process control
+
 strictMap :: (a -> b) -> [a] -> [b]
 strictMap fn (v:vs) =
   seq vs' (seq v' (v':vs'))
@@ -21,17 +23,14 @@ parallelMap fn (v:vs) =
         vs' = parallelMap fn vs
 parallelMap fn [] = []
 
-
 strictAppend [] l = l
 strictAppend (v:vs) l =
   seq vs' (v : vs')
   where vs' = strictAppend vs l
 
-
 strictFlatten (l:ls) =
   strictAppend l (strictFlatten ls)
 strictFlatten [] = []
-
 
 chunked chunksize lis =
   ch chunksize lis
@@ -41,37 +40,26 @@ chunked chunksize lis =
     where (lfront:ls) = ch (s-1) vs
   ch s [] = [[]]
 
-
 parallel_forM (i:is) fn =
   fn_i `par` (fn_is `pseq` (fn_i >> fn_is))
   where fn_i = fn i
         fn_is = parallel_forM is fn
-
 parallel_forM [] fn =
   return ()
 
 chunkedParallelForM chunksize is fn =
   parallel_forM (chunked chunksize is) (\is -> forM_ is fn)
 
-
 chunkedParallelMap chunksize fn vs =
   strictFlatten $ parallelMap (\chunk -> strictMap fn chunk) (chunked chunksize vs)
 
-intersection f g a = f a && g a
 
-distsquare (a,b) = 
-  a*a + b*b
+-- missing function combinator
 
-isDiverged x = (distsquare x) > (1e10**2)
+both f g a = f a && g a
 
-divergeDepth maxdepth seq =
-  fst (head (dropWhile (intersection (not . isDiverged . snd)
-                                     ((< maxdepth) . fst))
-                       (zip [1..] seq)))
 
-inscreen :: Int -> Int -> Int -> Double -> Double -> Double
-inscreen from to i fromr tor =
-  fromr + (tor - fromr) * (fromIntegral (i - from)) / (fromIntegral (to - from))
+-- Complex numbers
 
 square :: (Double,Double) -> (Double,Double)
 square (a,b) = 
@@ -80,13 +68,32 @@ square (a,b) =
 plus :: (Double,Double) -> (Double,Double) -> (Double,Double)
 plus (a1,a2) (b1,b2) = (a1+b1, a2+b2)
 
+-- distance^2 from (0,0)
+distsquare (a,b) = 
+  a*a + b*b
+
+-- Mandelbrot series
+
 mandelseries :: (Double, Double) -> [(Double, Double)]
 
 piter :: (Double,Double) -> (Double,Double) -> (Double,Double)
-piter c z = plus (square z) c
+piter c z = (square z) `plus` c
 
 mandel_ c z =  z : mandel_ c (piter c z)
 mandelseries c = mandel_ c (0.0,0.0)
+
+-- and its presentation
+
+isDiverged x = (distsquare x) > (1e10**2)
+
+divergeDepth maxdepth seq =
+  fst (head (dropWhile (both (not . isDiverged . snd)
+                             ((< maxdepth) . fst))
+                       (zip [1..] seq)))
+
+inscreen :: Int -> Int -> Int -> Double -> Double -> Double
+inscreen from to i fromr tor =
+  fromr + (tor - fromr) * (fromIntegral (i - from)) / (fromIntegral (to - from))
 
 
 depth = 90
