f0aa45bdf048925627b14af55b6fa789960600fa
Christian Jaeger <chrjae@gmail.com>
2013-11-07 17:07:22 +0000

    fix

--- c/x_posix_memalign.h
+++ c/x_posix_memalign.h
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 static void* x_posix_memalign(size_t alignment, size_t size) {
-    void *p=NULL; /* huh, avoid uninitialized warning */
-    int err= posix_memalign(p, alignment, size);
+    void *p;
+    int err= posix_memalign(&p, alignment, size);
     if (err) {
 	fprintf(stderr, "fatal: posix_memalign: %s\n", strerror(err));
 	exit(1);
